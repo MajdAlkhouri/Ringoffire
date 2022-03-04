2929c12c7b3cb8923387663fb9a5056a31010c87
MajdAlkhouri <mjodizaodiz@gmail.com>
2022-03-04 17:03:39 +0100

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBPgALVwesQW9z99P7f1IxgUf7IqKWWg1U",
+    authDomain: "ring-of-fire-c227e.firebaseapp.com",
+    projectId: "ring-of-fire-c227e",
+    storageBucket: "ring-of-fire-c227e.appspot.com",
+    messagingSenderId: "770547111367",
+    appId: "1:770547111367:web:1eb852e2841d6400a7b5e2",
+  }
 };
 
 /*
